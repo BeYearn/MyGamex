fc5dab872c1bc5e936afeb30e07ff3a62366afa6
BY <beyearn@gmail.com>
2017-06-30 17:09:17 +0800

    newMHR 合并common, 并 移除实现中的心跳绑定（在更新弱账户中实现）！！   而不再用登录成功发广播的形式

--- emasdklib/src/main/java/com/emagroup/sdk/impl/EmaUtilsImpl.java
+++ emasdklib/src/main/java/com/emagroup/sdk/impl/EmaUtilsImpl.java
@@ -3,7 +3,6 @@ package com.emagroup.sdk.impl;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -16,9 +15,7 @@ import com.emagroup.sdk.EmaPay;
 import com.emagroup.sdk.EmaPayInfo;
 import com.emagroup.sdk.EmaSDKListener;
 import com.emagroup.sdk.EmaSDKUser;
-import com.emagroup.sdk.EmaService;
 import com.emagroup.sdk.EmaUser;
-import com.emagroup.sdk.EmaUtils;
 import com.emagroup.sdk.EmaUtilsInterface;
 import com.emagroup.sdk.HttpRequestor;
 import com.emagroup.sdk.InitCheck;
@@ -101,10 +98,6 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
                         String nikename = ret.nick_name;
                         EmaUser.getInstance().setAllianceUid(uid + "");
                         EmaUser.getInstance().setNickName(nikename);
-
-                        //绑定服务
-                        Intent serviceIntent = new Intent(mActivity, EmaService.class);
-                        mActivity.bindService(serviceIntent, EmaUtils.getInstance(mActivity).mServiceCon, Context.BIND_AUTO_CREATE);
 
                         //补充弱账户信息
                         EmaSDKUser.getInstance(mActivity).updateWeakAccount(listener, ULocalUtils.getAppId(mActivity), ULocalUtils.getChannelId(mActivity), ULocalUtils.getChannelTag(mActivity), ULocalUtils.getDeviceId(mActivity), EmaUser.getInstance().getAllianceUid());
