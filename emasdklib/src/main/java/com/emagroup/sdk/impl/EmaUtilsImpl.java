4ad3d9d25d76bb78f5b7ebb4b7eb4d6ab7d7f996
BY <beyearn@gmail.com>
2017-09-07 15:21:21 +0800

    newCX 创星基本接完

--- emasdklib/src/main/java/com/emagroup/sdk/impl/EmaUtilsImpl.java
+++ emasdklib/src/main/java/com/emagroup/sdk/impl/EmaUtilsImpl.java
@@ -4,13 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 
 import com.emagroup.sdk.EmaBackPressedAction;
+import com.emagroup.sdk.EmaCallBackConst;
+import com.emagroup.sdk.EmaConst;
+import com.emagroup.sdk.EmaPay;
 import com.emagroup.sdk.EmaPayInfo;
 import com.emagroup.sdk.EmaSDKListener;
+import com.emagroup.sdk.EmaUser;
+import com.emagroup.sdk.EmaUtils;
 import com.emagroup.sdk.EmaUtilsInterface;
+import com.emagroup.sdk.ULocalUtils;
 import com.emagroup.sdk.Url;
+import com.yyjia.sdk.center.GMcenter;
+import com.yyjia.sdk.data.Information;
+import com.yyjia.sdk.listener.InitListener;
+import com.yyjia.sdk.listener.LoginListener;
+import com.yyjia.sdk.listener.PayListener;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -21,6 +33,7 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
 
     private static EmaUtilsImpl instance;
     private Activity mActivity;
+    private GMcenter mCenter;
 
 
     public static EmaUtilsImpl getInstance(Activity activity) {
@@ -36,7 +49,54 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
 
     @Override
     public void immediateInit(EmaSDKListener listener) {
+        mCenter = GMcenter.getInstance(mActivity);
+        mCenter.onCreate(mActivity);
 
+        mCenter.setInitListener(new InitListener() {
+            @Override
+            public void initSuccessed(String code) {
+                if (code == Information.INITSUCCESSEDS) {
+                    //初始化成功调用登录接口
+                    //mCenter.checkLogin();
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.INITSUCCESS, "初始化成功", null);
+                } else {
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.INITFALIED, "初始化失败", null);
+                }
+            }
+        });
+
+        mCenter.setLoginListener(new LoginListener() {
+            @Override
+            public void loginSuccessed(String code) {
+                if (code == Information.LOGIN_SUSECCEDS) {
+                    //String sid = mCenter.getSid();//取得sessionid
+                    int sid =mCenter.getUid();
+                    HashMap<String, String> userData = new HashMap<>();
+                    userData.put(EmaConst.ALLIANCE_UID, sid+"");
+                    userData.put(EmaConst.NICK_NAME, "");
+
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINSUCCESS_CHANNEL, "渠道登录成功", userData);
+                } else {
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINFALIED, "登录失败", null);
+                }
+            }
+
+            @Override
+            public void logcancelSuccessed(String code) {
+                if (code == Information.LOGCANCEL_SUSECCED) {
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINCANELL, "登录取消", null);
+                }
+            }
+
+            @Override
+            public void logoutSuccessed(String code) {
+                if (code == Information.LOGOUT_SUSECCED) {
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGOUTSUCCESS, "登出成功", null);
+                } else {
+                    EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGOUTFALIED, "登出失败", null);
+                }
+            }
+        });
     }
 
     @Override
@@ -46,7 +106,12 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
 
     @Override
     public void realLogin(final EmaSDKListener listener, String userid, String deviceId) {
-
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mCenter.checkLogin();
+            }
+        });
     }
 
     /**
@@ -60,13 +125,43 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
     }
 
     @Override
-    public void realPay(final EmaSDKListener listener, EmaPayInfo emaPayInfo) {
+    public void realPay(final EmaSDKListener listener, final EmaPayInfo emaPayInfo) {
 
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                Float money = Float.valueOf(emaPayInfo.getPrice());
+                String productname = emaPayInfo.getProductName();
+                String serverId = (String) ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ZONE_ID, "");
+                String charId = EmaUser.getInstance().getAllianceUid();
+                String cporderId = emaPayInfo.getOrderId();
+                String callbackInfo = "ext";
+
+                mCenter.pay(mActivity, money, productname, serverId, charId, cporderId, callbackInfo, new PayListener() {
+                    @Override
+                    public void paySuccessed(String code, String cporderid) {
+                        if (code == Information.PAY_SUSECCED) {
+                            listener.onCallBack(EmaCallBackConst.PAYSUCCESS, "购买成功");
+                        } else {
+                            listener.onCallBack(EmaCallBackConst.PAYFALIED, "支付失败");
+                        }
+                    }
+
+                    @Override
+                    public void payGoBack() {
+                        listener.onCallBack(EmaCallBackConst.PAYCANELI, "支付取消");
+                        EmaPay.getInstance(mActivity).cancelOrder();
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
     public void logout() {
-
+        mCenter.logout();
     }
 
     @Override
@@ -76,14 +171,19 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
 
     @Override
     public void doShowToolbar() {
+        mCenter.showFloatingView();
     }
 
     @Override
     public void doHideToobar() {
+        mCenter.hideFloatingView();
     }
 
     @Override
     public void onResume() {
+        if (mCenter != null) {
+            mCenter.showFloatingView();
+        }
     }
 
     @Override
@@ -92,6 +192,9 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
 
     @Override
     public void onStop() {
+        if (mCenter != null) {
+            mCenter.hideFloatingView();
+        }
     }
 
     @Override
@@ -100,12 +203,19 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
 
     @Override
     public void onBackPressed(final EmaBackPressedAction action) {
-
+        action.doBackPressedAction();
+        mCenter.exitGame();
     }
 
     @Override
     public void submitGameRole(Map<String, String> data) {
-
+        String serverId = data.get(EmaConst.SUBMIT_ZONE_ID);
+        String serverName = data.get(EmaConst.SUBMIT_ZONE_NAME);
+        String roleId = data.get(EmaConst.SUBMIT_ROLE_ID);
+        String roleName = data.get(EmaConst.SUBMIT_ROLE_NAME);
+        String roleLevel = data.get(EmaConst.SUBMIT_ROLE_LEVEL);
+        String roleCTime = data.get(EmaConst.SUBMIT_ROLE_CT);
+        mCenter.submitRoleInfo(serverId, serverName, roleId, roleName, roleLevel, roleCTime);
     }
 
     @Override
