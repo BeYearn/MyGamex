c6654d394f109cc46c02822b9a57f192a0cea7e1
BY <beyearn@gmail.com>
2017-09-19 14:16:11 +0800

    newWM submitGamexx dataType 1/2都上传

--- emasdklib/src/main/java/com/emagroup/sdk/impl/EmaUtilsImpl.java
+++ emasdklib/src/main/java/com/emagroup/sdk/impl/EmaUtilsImpl.java
@@ -31,7 +31,6 @@ import java.util.Map;
 
 /**
  * Created by Administrator on 2016/10/9.
- *
  */
 public class EmaUtilsImpl implements EmaUtilsInterface {
 
@@ -63,16 +62,16 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
             String channelAppSecret = data.getString("channelAppSecret");
 
             WMPlatform.getInstance().init(mActivity,
-                    isLandScape? WMPlatform.WMPlatformDirection.Landscape: WMPlatform.WMPlatformDirection.Portrait,
+                    isLandScape ? WMPlatform.WMPlatformDirection.Landscape : WMPlatform.WMPlatformDirection.Portrait,
                     channelAppKey,
                     channelAppSecret,
                     ULocalUtils.getApplicationName(mActivity),
                     mActivity.getPackageName()
             );
 
-            EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.INITSUCCESS, "初始化成功",null);
+            EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.INITSUCCESS, "初始化成功", null);
         } catch (JSONException e) {
-            EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.INITFALIED, "初始化失败",null);
+            EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.INITFALIED, "初始化失败", null);
             e.printStackTrace();
         }
     }
@@ -86,12 +85,12 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
             public void onLoginFailed(WMError error) {
                 //登录失败执行此回调
                 //失败信息 error.message
-                Log.e("WMLoginFail",error.message);
-                EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINFALIED, "登陆失败回调",null);
+                Log.e("WMLoginFail", error.message);
+                EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINFALIED, "登陆失败回调", null);
             }
 
             @Override
-            public void onLoginCompleted(WMUser user){
+            public void onLoginCompleted(WMUser user) {
                 //登录成功执行此回调 用户信息的访问方式如下：
                 //用户唯一标识 user.userId
                 //用户帐号 user.userName
@@ -99,10 +98,10 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
                 //时间戳秒数   user.tokenTime
                 //验证加密串   user.token
                 HashMap<String, String> userData = new HashMap<>();
-                userData.put(EmaConst.ALLIANCE_UID,user.userId);
-                userData.put(EmaConst.NICK_NAME,user.userName);
+                userData.put(EmaConst.ALLIANCE_UID, user.userId);
+                userData.put(EmaConst.NICK_NAME, user.userName);
 
-                EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINSUCCESS_CHANNEL, "渠道登录成功",userData);
+                EmaUtils.getInstance(mActivity).makeUserCallBack(EmaCallBackConst.LOGINSUCCESS_CHANNEL, "渠道登录成功", userData);
             }
         });
 
@@ -122,18 +121,18 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
     public void realPay(final EmaSDKListener listener, EmaPayInfo emaPayInfo) {
 
         WMPayInfo payInfo = new WMPayInfo();
-        payInfo.setAmount(emaPayInfo.getPrice()+"");//支付金额  单位：元(人民币) 注：正整数 请勿使用小数点
+        payInfo.setAmount(emaPayInfo.getPrice() + "");//支付金额  单位：元(人民币) 注：正整数 请勿使用小数点
         payInfo.setName(emaPayInfo.getProductName());//设置购买货币单位名称
-        payInfo.setServerId(ULocalUtils.spGet(mActivity,EmaConst.SUBMIT_ZONE_NAME,"100")+"");//设置区服
+        payInfo.setServerId(ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ZONE_NAME, "100") + "");//设置区服
         payInfo.setOrderId(emaPayInfo.getOrderId());//设置订单号 请勿重复
-        payInfo.setDescription(emaPayInfo.getProductNum()+"份");//货币数量描述，不要带货币单位
+        payInfo.setDescription(emaPayInfo.getProductNum() + "份");//货币数量描述，不要带货币单位
         payInfo.setExtendInfo("extend_info");//扩展字段，发货通知时会回传（app_ext字段）
 
         WMPlatform.getInstance().doPay(payInfo, new WMPayListener() {
             @Override
             public void onPayFailed(WMError error) {
                 // 支付失败时回调此函数 应用在此函数中接管用户交互
-                Log.e("WMPayFail",error.message);
+                Log.e("WMPayFail", error.message);
                 listener.onCallBack(EmaCallBackConst.PAYFALIED, "支付失败");
             }
 
@@ -189,18 +188,16 @@ public class EmaUtilsImpl implements EmaUtilsInterface {
     @Override
     public void submitGameRole(Map<String, String> data) {
         String dataType = data.get(EmaConst.SUBMIT_DATA_TYPE);
-        if("1".equals(dataType)){
 
-            HashMap<String, String> map = new HashMap<>();
-            map.put("uid", EmaUser.getInstance().getAllianceUid());
-            map.put("sid",ULocalUtils.spGet(mActivity,EmaConst.SUBMIT_ZONE_ID,"")+"");
-            map.put("sname",ULocalUtils.spGet(mActivity,EmaConst.SUBMIT_ZONE_NAME,"")+"");
-            map.put("roleid",ULocalUtils.spGet(mActivity,EmaConst.SUBMIT_ROLE_ID,"")+"");
-            map.put("rolename",ULocalUtils.spGet(mActivity,EmaConst.SUBMIT_ROLE_NAME,"")+"");
-            map.put("rolelevel",ULocalUtils.spGet(mActivity,EmaConst.SUBMIT_ROLE_LEVEL,"")+"");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("uid", EmaUser.getInstance().getAllianceUid());
+        map.put("sid", ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ZONE_ID, "") + "");
+        map.put("sname", ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ZONE_NAME, "") + "");
+        map.put("roleid", ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ROLE_ID, "") + "");
+        map.put("rolename", ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ROLE_NAME, "") + "");
+        map.put("rolelevel", ULocalUtils.spGet(mActivity, EmaConst.SUBMIT_ROLE_LEVEL, "") + "");
 
-            WMPlatform.statistics("stat/role", map);
-        }
+        WMPlatform.statistics("stat/role", map);
     }
 
     @Override
